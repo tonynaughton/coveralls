4bedbc16216b2d421d2b26122a412638a09caa93
TonyN96 <tonynaughton1@gmail.com>
2024-10-11 17:50:11 +0200

    added untested text

--- src/Greeting.test.tsx
+++ src/Greeting.test.tsx
@@ -6,7 +6,7 @@ import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
 test('renders the correct greeting message', () => {
-  render(<Greeting name="Tony" country="Ireland" />);
-  const greetingElement = screen.getByText(/Hello, Tony!/i);
+  render(<Greeting name="John" country="Ireland" />);
+  const greetingElement = screen.getByText(/Hello, John!/i);
   expect(greetingElement).toBeInTheDocument();
 });
