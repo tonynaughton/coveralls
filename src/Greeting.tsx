57aae0f43cce5c02b59983bb2f070e26042cf62c
TonyN96 <tonynaughton1@gmail.com>
2024-10-11 17:41:18 +0200

    added untested text

--- src/Greeting.tsx
+++ src/Greeting.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 type GreetingProps = {
   name: string;
+  country: string;
 };
 
-const Greeting: React.FC<GreetingProps> = ({ name }) => {
-  return <h1>Hello, {name}!</h1>;
+const Greeting: React.FC<GreetingProps> = ({ name, country }) => {
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <h1>You're from {country}</h1>
+    </div>
+  );
 };
 
 export default Greeting;
